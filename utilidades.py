c22d8bfc5babae28a39d1887f6079a78d958ef3a
pr0nstar <psiport@hotmail.com>
2020-04-29 02:02:34 -0400

    Datos generales

--- utilidades.py
+++ utilidades.py
@@ -41,20 +41,50 @@ def run(model_fn, model_data, days, acc_days=0,  step=0.1, **params):
 
 
 pyplot.rcParams['figure.figsize'] = [50/2.54, 22/2.54]
-def plot(x, *curves):
+def plot(x, *curves, **kwargs):
     fig, ax = pyplot.subplots()
-    for curve in curves:
-        ax.plot(x[:len(curve)], curve)
+    for idx, curve in enumerate(curves):
+        label = kwargs['labels'][idx] if 'labels' in kwargs else None
+        ax.plot(x[:len(curve)], curve, label=label)
 
     pyplot.grid(b=True, color='DarkTurquoise', alpha=0.2, linestyle=':', linewidth=2)
+
+    if 'labels' in kwargs:
+        ax.legend(loc='upper left')
 
     return ax
 
 # https://github.com/mauforonda
-def load_data(path='./data/covid19-bolivia2/nacional.csv'):
-    with open(path) as f:
+def load_data():
+    final_data = []
+
+    with open('./data/covid19-bolivia2/nacional.csv') as f:
         csv_file = csv.reader(f)
         data = [[int(_) for _ in line[1:]] for idx, line in enumerate(csv_file) if idx > 0]
-        data = zip(*data)
 
-    return list(data)
+        final_data.extend(zip(*data))
+
+    with open('./data/covid19-bolivia/descartados.csv') as f:
+        csv_file = csv.reader(f)
+        data = [[int(_) for _ in line[1:]] for idx, line in enumerate(csv_file) if idx > 0]
+        data = [sum(_) for _ in data]
+        data = np.pad(data, (0, 12), 'constant', constant_values=(0,))
+        data = np.array([data[_] - data[_ + 1] for _ in range(len(data) - 1)], dtype='float64')
+
+        # No hay una forma mas facil?
+        zeros = np.flatnonzero(data == 0)
+        zeros_counts = np.diff(np.flatnonzero(np.concatenate(
+            ([True], (zeros[1:] - 1) != zeros[:-1], [True])
+        )))
+
+        acc = 0
+        for zero_count in zeros_counts:
+            pos = zeros[acc]
+            if pos > 0:
+                data[pos - 1:pos + zero_count] = data[pos - 1] / (zero_count + 1.)
+            acc = acc + zero_count
+
+        data = np.flip(data) + np.diff(final_data[1], prepend=[final_data[1][0]])
+        final_data.append(data)
+
+    return final_data
